4e5b5439b5add21a3bea9ee0e65090aef745e318
ybowdotio <office@ybow.io>
2025-07-01 14:28:27 -0500

    Update syncCallLogs.js

--- netlify/functions/syncCallLogs.js
+++ netlify/functions/syncCallLogs.js
@@ -1,3 +1,13 @@
+const axios = require('axios');
+const { createClient } = require('@supabase/supabase-js');
+
+const HUBSPOT_API_KEY = process.env.HUBSPOT_APP_TOKEN;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+console.log('HubSpot token exists?', !!process.env.HUBSPOT_APP_TOKEN);
+
 exports.handler = async function () {
   try {
     const contactsResp = await axios.get(
